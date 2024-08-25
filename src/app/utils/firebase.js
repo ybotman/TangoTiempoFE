4f06896a18511450b93e08e566ae8561f53e3a96
Tural Novruzov <tural.nt.1@gmail.com>
2024-08-25 11:48:37 -0400

    Workaround for firebase env variables

--- src/app/utils/firebase.js
+++ src/app/utils/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    apiKey: "AIzaSyBbyJuxuCIY-BwzLPItpQbtegAkAMo755o",
+    authDomain: "tangotiempo-257ff.firebaseapp.com",
+    projectId: "tangotiempo-257ff",
+    storageBucket: "tangotiempo-257ff.appspot.com",
+    messagingSenderId: "685681979859",
+    appId: "1:685681979859:web:6609aa591ea8917166bf26",
+    measurementId: "G-8DED6NXCJ8",
 };
 
 const app = initializeApp(firebaseConfig);
