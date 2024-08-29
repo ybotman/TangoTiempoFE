696ab6ab9c57499787a6a067312de607946cc21e
YbotMan <toby.balsley+YbotMan@gmail.com>
2024-08-29 15:25:37 -0400

    ABC

--- src/app/components/UI/SiteHeader.js
+++ src/app/components/UI/SiteHeader.js
@@ -2,15 +2,18 @@ import React from 'react';
 import Image from 'next/image';
 
 const SiteHeader = () => {
+    // Assuming run_number is passed as a prop or can be fetched in this component
+    const runNumber = process.env.NEXT_PUBLIC_BUILD_VERSION || 'Local'; // Fallback value if not set
+
     return (
         <div style={{ position: 'relative', width: '100%', height: 'auto' }}>
             <Image
                 src="/TangoTiempoColorFull.png"
                 alt="Tango Tiempo"
-                width={1200} // specify a width
-                height={600} // specify a height
-                style={{ width: '100%', height: 'auto' }} // keep aspect ratio
-                priority // for loading optimization
+                width={1200}
+                height={600}
+                style={{ width: '100%', height: 'auto' }}
+                priority
             />
             <a
                 href="https://www.buymeacoffee.com/ybotman"
@@ -20,17 +23,30 @@ const SiteHeader = () => {
                     position: 'absolute',
                     top: '10px',
                     left: '10px',
-                    backgroundColor: 'rgba(0, 255, 255, 0.7)', // Cyan background with 30% transparency
+                    backgroundColor: 'rgba(0, 255, 255, 0.7)',
                     color: '#000',
-                    padding: '5px 10px', // Smaller padding
-                    borderRadius: '3px', // Flatter border radius
+                    padding: '5px 10px',
+                    borderRadius: '3px',
                     textDecoration: 'none',
                     fontWeight: 'normal',
-                    fontSize: '12px', // Smaller font size
+                    fontSize: '12px',
                 }}
             >
-                Share a Tanda
+                Gift an Empanada
             </a>
+            <div
+                style={{
+                    position: 'absolute',
+                    top: '10px',
+                    width: '100%',
+                    textAlign: 'center',
+                    color: 'white',
+                    fontSize: '12px',
+                    opacity: '0.8', // Slight transparency to keep it inconspicuous
+                }}
+            >
+                {runNumber}
+            </div>
         </div>
     );
 };
