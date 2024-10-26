bf2833ab19a73f2c32011066daff158c809bca9c
YbotMan <toby.balsley+YbotMan@gmail.com>
2024-10-26 16:20:45 -0400

    ELSINT updates

---
+++ .eslintrc.config.js
@@ -0,0 +1,48 @@
+// eslint.config.js
+
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import prettier from 'eslint-plugin-prettier';
+import next from 'eslint-plugin-next';
+
+export default [
+  js.configs.recommended,
+  next.configs['core-web-vitals'],
+  react.configs.recommended,
+  prettier.configs.recommended,
+  {
+    plugins: {
+      react,
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'error', // Enable PropTypes linting
+      'no-unused-vars': 'warn',
+      'no-undef': 'error',
+    },
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+    },
+    env: {
+      browser: true,
+      node: true,
+      es6: true,
+    },
+    ignores: [
+      'node_modules/',
+      '.next/',
+      'public/',
+      'dist/',
+      'logs/',
+      '*.log',
+      '.env',
+      '.env.local',
+      '.env.development',
+      '.env.production',
+      'archive/',
+    ],
+  },
+];
