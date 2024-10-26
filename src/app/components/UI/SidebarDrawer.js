aba95fe5dd103689736b63b1886f37a599c91767
YbotMan <toby.balsley+YbotMan@gmail.com>
2024-10-26 15:05:39 -0400

    Hidding Side MenuBar

--- src/app/components/UI/SidebarDrawer.js
+++ src/app/components/UI/SidebarDrawer.js
@@ -14,6 +14,7 @@ import {
 import MenuIcon from '@mui/icons-material/Menu';
 import InfoIcon from '@mui/icons-material/Info';
 import HelpIcon from '@mui/icons-material/Help';
+
 import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
 
 const SidebarDrawer = ({ open, onClose }) => {
