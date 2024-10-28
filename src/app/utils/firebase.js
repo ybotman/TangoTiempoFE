ac91aad0b8d274ea393bef3f257e076d108aaad9
YbotMan <toby.balsley+YbotMan@gmail.com>
2024-10-28 13:49:28 -0400

    firebase back to old

--- src/app/utils/firebase.js
+++ src/app/utils/firebase.js
@@ -3,22 +3,16 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-let decodedFirebaseConfig;
+// Decode the Base64 encoded JSON string from the environment variable
+const decodedFirebaseConfig = JSON.parse(
+  Buffer.from(process.env.NEXT_PUBLIC_FIREBASE_JSON, 'base64').toString('utf-8')
+);
 
-if (typeof window === 'undefined') {
-  // Server-side decoding of Base64 string
-  decodedFirebaseConfig = JSON.parse(
-    Buffer.from(process.env.NEXT_PUBLIC_FIREBASE_JSON, 'base64').toString(
-      'utf-8'
-    )
-  );
-} else {
-  // Client-side error handling (optional)
-  console.error('Firebase config can only be decoded on the server side.');
-}
+console.log('Firebase Config:', decodedFirebaseConfig); // Debugging line
 
-// Initialize Firebase app only if decodedFirebaseConfig is available
-const app = decodedFirebaseConfig ? initializeApp(decodedFirebaseConfig) : null;
+// Initialize Firebase app
+const app = initializeApp(decodedFirebaseConfig);
 
-// Export auth for use in other components (null if on client without config)
-export const auth = app ? getAuth(app) : null;
+// Export auth for use in other components
+export const auth = getAuth(app);
+ 
