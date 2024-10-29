829b32ef3db4583d2d445cf7233ac42d99b245fc
YbotMan <toby.balsley+YbotMan@gmail.com>
2024-10-29 10:38:31 -0400

    FaceBook MetaTag

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* ... other head elements */}
+        {/* Facebook Domain Verification */}
+        <meta name="facebook-domain-verification" content="39o5h74oasigr7d761a9danhuv0ckg" />
+
+        {/* Google Analytics Script */}
         <Script
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
           strategy="afterInteractive"
