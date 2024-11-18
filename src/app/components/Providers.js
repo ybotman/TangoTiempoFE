d99cc11a421a2ed8be454ee434ff02e89af5eb37
YbotMan <toby.balsley+YbotMan@gmail.com>
2024-11-18 13:37:34 -0500

    clean up for 01-Mini-Facebook-and-Mulit-Provider

--- src/app/components/Providers.js
+++ src/app/components/Providers.js
@@ -26,37 +26,3 @@ Providers.propTypes = {
 };
 
 export default Providers;
-
-/* // src/app/components/Providers.js
-
-'use client';
-
-import React from 'react';
-import PropTypes from 'prop-types';
-import { AuthProvider } from '@/contexts/AuthContext';
-import { RegionsProvider } from '@/contexts/RegionsContext';
-import { RoleProvider } from '@/contexts/RoleContext';
-import { AdapterLuxon } from '@mui/x-date-pickers/AdapterLuxon';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { Settings } from 'luxon';
-
-const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-Settings.defaultZone = timezone;
-const Providers = ({ children }) => {
-  return (
-    <AuthProvider>
-      <RegionsProvider>
-        <LocalizationProvider dateAdapter={AdapterLuxon}>
-          <RoleProvider>{children}</RoleProvider>
-        </LocalizationProvider>
-      </RegionsProvider>
-    </AuthProvider>
-  );
-};
-
-Providers.propTypes = {
-  children: PropTypes.node.isRequired,
-};
-
-export default Providers;
-*/
