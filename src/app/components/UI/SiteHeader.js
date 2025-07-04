aaac212228c12c46596ebbd18be5e087d599cd6d
YbotMan (TobyBalsley) <toby.balsley@gmail.com>
2025-07-04 01:31:02 -0400

    Make SiteHeader read version dynamically from package.json

--- src/app/components/UI/SiteHeader.js
+++ src/app/components/UI/SiteHeader.js
@@ -7,6 +7,7 @@ import { RoleContext } from '@/contexts/RoleContext';
 import { AuthContext } from '@/contexts/AuthContext';
 import { useOrganizers } from '@/hooks/useOrganizers';
 import LocationContextModal from '@/components/Modals/misc/LocationContextModal';
+import packageJson from '../../../../package.json';
 
 const SiteHeader = () => {
   const { selectedLocation } = useGeoLocation();
@@ -14,7 +15,7 @@ const SiteHeader = () => {
   const { user } = useContext(AuthContext);
   const { organizer, fetchOrganizerById } = useOrganizers();
   const [locationModalOpen, setLocationModalOpen] = useState(false);
-  const appVersion = 'v1.1.0'; // From package.json
+  const appVersion = `v${packageJson.version}`; // Dynamically read from package.json
   
   // Fetch organizer data when user is a RegionalOrganizer
   useEffect(() => {
